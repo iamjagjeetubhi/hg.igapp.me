cf7e269b80ec42ab26819030df4a38644db4e7dc
root <iamjagjeetubhi@gmail.com>
2018-01-19 08:05:21 +0000

    fixed

---
+++ mysite/urls.py
@@ -0,0 +1,37 @@
+from django.conf.urls import include, url, handler404
+from django.contrib import admin
+# Static helper function only for development!
+from django.conf.urls.static import static
+from django.conf import settings
+from . import views
+from links import views
+
+
+
+from django.contrib.auth import views as auth_views
+handler404 = 'links.views.index'
+
+
+urlpatterns = [
+
+    url(r'^user/$', views.userlink, name='user'),
+ 
+	url(r'^password_reset/$', auth_views.password_reset, name='password_reset'),
+    url(r'^password_reset/done/$', auth_views.password_reset_done, name='password_reset_done'),
+    url(r'^reset/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$',
+        auth_views.password_reset_confirm, name='password_reset_confirm'),
+    url(r'^reset/done/$', auth_views.password_reset_complete, name='password_reset_complete'),
+
+    url(r'^(?P<username>([^/]+))/$', views.myprofileview, name="myprofileview"),
+
+
+
+    url(r'^$', views.index, name="indexview"),
+    
+    url(r'^admin/', admin.site.urls),
+    url(r'^oauth/', include('social_django.urls', namespace='social')),
+	url(r'^avatar/', include('avatar.urls')),
+]
+
+if settings.DEBUG is True:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) 
